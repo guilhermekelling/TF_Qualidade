d75cb370bf2511e6772ad41eb9c84f4a02a8216b
guilhermekelling <guilhermekelling@gmail.com>
2017-03-25 08:35:09 -0300

    Primeiro git

---
+++ src/pagina/Driver.java
@@ -0,0 +1,52 @@
+package pagina;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+	private WebDriver driver;
+	private String baseUrl;
+	private PaginaLogin paginaLogin;
+	
+	
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+
+	public void setBaseUrl(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+
+	public Driver() {
+		System.setProperty("webdriver.firefox.marionette","C:\\plugin\\geckodriver.exe");	
+		this.driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	public void navegateTo(String url) {
+		driver.navigate().to(url);
+	}
+	
+	public WebDriver getDriver() {
+		return driver;
+	}
+	
+	public void closeNavegator() {
+		getDriver().close();
+	}
+	
+	public void setTimeouts(int time){		
+		driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
+	}
+	
+	public void executarLogin(String email, String senha){
+		paginaLogin = new PaginaLogin(email,senha);
+		paginaLogin.executar(driver);
+	}
+	
+	public void acessarPaginaLogin(){
+		driver.get(baseUrl+ "/login");
+	}
+}
---
+++ src/pagina/PaginaInicial.java
@@ -0,0 +1,8 @@
+package pagina;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PaginaInicial {
+
+}
