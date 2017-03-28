1e1d08309fe773a54b706c78f65952941d795895
Guilherme Kelling <guilhermekelling@gmail.com>
2017-03-28 16:41:01 -0300

    Alteração do travis.yml para usar o phantomjs. Alteração da classe Driver.java para configurar o driver de acordo com o sistema operacional. Alteração no bluid.xlm para corrigir o caminho do teste

--- src/pagina/Driver.java
+++ src/pagina/Driver.java
@@ -5,6 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Driver {
 	private WebDriver driver;
@@ -21,9 +24,21 @@ public class Driver {
 	}
 
 	public Driver() {
-		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");	
+		//System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");	
 		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe"); 
-		this.driver = new ChromeDriver();
+		//this.driver = new ChromeDriver();
+		String OS = System.getProperty("os.name").toLowerCase();
+
+		if(OS.indexOf("win") >= 0){
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe"); 
+			driver = new ChromeDriver();
+		} else {
+			//System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver")
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/local/bin/phantomjs");                  
+			driver = new PhantomJSDriver(cap);		
+		}		
+		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
